b7421e3651f260f8bd68735fd3ae2f11e8931e50
kaeptnkrunch <55900780+kaeptnkrunch@users.noreply.github.com>
2020-09-03 14:00:15 +0200

    Add files via upload

---
+++ fehlzeitquote.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Fehlzeitquoten Rechner von Stephan Langenau"
+$datum = Get-Date 
+Write-Host "Heute haben wir den $datum"
+$Input1 = Read-Host "Gib deine aktuellen Fehltage an"
+$Input2 = Read-Host "Gib deine aktuellen Arbeitstage an"
+$quote = ($Input1 / $Input2) * 100 
+Write-Host "Deine aktuelle Fehlzeitquote am $datum ist $quote"
